ec05571de0f44ad24efd7303812e5d31ec147cdc
localAccess <localAccess@DESKTOP-F3QREPU>
2021-09-02 14:08:52 +0900

    초기 커밋.

---
+++ Engine/main.cpp
@@ -0,0 +1,7 @@
+// KNU CSE 2021****** Jeongwoo Kim
+// (c) 2021 Jeongwoo Kim
+// The idea of this program is based on Material System provided by Source Engine by Valve Software.
+// Material System
+#include <stdio.h>
+#include <stdlib.h>
+#include "material_datastructure.h"
